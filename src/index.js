e32816bdce29fa7b61f89623fbe0c2c96c34cada
Kunionick <carpediem.sbr7@gmail.com>
2021-08-23 23:29:36 +0900

    reactチュートリアル_課題①-⑥追加

--- src/index.js
+++ src/index.js
@@ -4,40 +4,40 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    //課題⑤変更・追加
+    <button className={`square ${props.isHighlight ? 'highlight' : ''}`} onClick={() => props.onClick()}>
       {props.value}
     </button>
   );
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+  renderSquare(i, isHighlight = false) {
+    //課題⑤変更
+    //課題⑤変更・追加
+    return <Square isHighlight={isHighlight} key={i} value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
   }
-
+  //課題③Boardをloopを2回使用に変更
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {Array(3)
+          .fill(0)
+          .map((row, i) => {
+            return (
+              <div className="board-row" key={i}>
+                {Array(3)
+                  .fill(0)
+                  .map((col, j) => {
+                    return this.renderSquare(i * 3 + j, this.props.highlightCells.indexOf(i * 3 + j) !== -1); //課題⑤追加
+                  })}
+              </div>
+            );
+          })}
       </div>
     );
   }
 }
-
 class Game extends React.Component {
   constructor(props) {
     super(props);
@@ -49,6 +49,8 @@ class Game extends React.Component {
       ],
       stepNumber: 0,
       xIsNext: true,
+      // 課題④追加
+      isAsc: true,
     };
   }
 
@@ -64,6 +66,8 @@ class Game extends React.Component {
       history: history.concat([
         {
           squares: squares,
+          col: Math.floor(i / 3) + 1, //課題①(col,row)表示
+          row: (i % 3) + 1, //課題①(col,row)表示
         },
       ]),
       stepNumber: history.length,
@@ -78,35 +82,61 @@ class Game extends React.Component {
     });
   }
 
+  //課題④遷移リストの昇順・降順
+  toggleAsc() {
+    this.setState({
+      asc: !this.state.asc,
+    });
+  }
+
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const settlement = calculateWinner(current.squares); ////課題⑤勝利につながった 3 つのマス目をハイライト
+
+    //課題⑤変更
+    let status;
+    if (settlement) {
+      //課題⑥追加
+      if (settlement.isDraw) {
+        status = 'Draw';
+      }else {
+        status = 'Winner: ' + settlement.winner; //変更
+      }
+      //課題⑥追加_ここまで
+    } else {
+      status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     const moves = history.map((step, move) => {
-      const desc = move ? 'Go to move #' + move : 'Go to game start';
+      const desc = move ? 'Move #' + move + '(' + step.col + ',' + step.row + ')' : 'Game Start'; //課題①(col,row)表示
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          {/* 課題②着手履歴リスト内で現在選択されているアイテムを太字表示。CSS編集有 */}
+          <button onClick={() => this.jumpTo(move)} className={this.state.stepNumber === move ? 'bold' : ''}>
+            {desc}
+          </button>
         </li>
       );
     });
 
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={current.squares} onClick={(i) => this.handleClick(i)} />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+            highlightCells={settlement ? settlement.line : []} //課題⑤追加
+          />
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{moves}</ol>
+          {/* 課題④遷移リストの昇順・降順 */}
+          <div>
+            <button onClick={() => this.toggleAsc()}>ASC ⇔ DESC</button>
+          </div>
+          <ol>{this.state.asc ? moves : moves.reverse()}</ol>
+          {/* <ol>{moves}</ol> */}
         </div>
       </div>
     );
@@ -131,8 +161,21 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+        isDraw: false, //課題⑥引き分け時draw表示
+        winner: squares[a], //課題⑤勝利につながった 3 つのマス目をハイライト
+        line: [a, b, c], //課題⑤勝利につながった 3 つのマス目をハイライト
+      };
     }
   }
+  //課題⑥追加
+  if (squares.filter((e) => !e).length === 0) {
+    return {
+      isDraw: true,
+      winner: null,
+      line: [],
+    };
+  }
+
   return null;
 }
